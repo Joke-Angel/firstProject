fc7940b312d0f2d046f44989fbcf1dd58ec31433
Jokex3 <joselu.langel20@gmail.com>
2021-05-02 15:26:19 -0400

    Agrego otra condicion

--- ejericioSimple.py
+++ ejericioSimple.py
@@ -4,3 +4,8 @@ if edad < 18:
     print ("Eres menor de edad!")
 else:
     print ("Eres mayor de edad!")
+
+if edad > 75:
+    print ("Debes tener cuidado por tu edad")
+else:
+    print ("Jovenzuelo")
